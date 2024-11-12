1c7ba7e70270028e4e6060e4cc6690fa1f89c9ed
Orcaa404 <curiositycodea99@gmail.com>
2024-11-12 13:03:15 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,13 @@
+//create an object  in javascript
+const student ={
+    fullName : "alfiya mulla",
+    age: 20,
+    subject: "javascript",
+    isPass: true,
+    
+    };
+    student.age = student.age + 1;
+    console.log(student.age);
+
+
+    
