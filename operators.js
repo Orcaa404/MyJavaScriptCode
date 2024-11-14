e9f0ac89c0315b84199b332c0ebb028c19073cfb
Orcaa404 <curiositycodea99@gmail.com>
2024-11-14 09:55:20 +0530

    new change operators

--- operators.js
+++ operators.js
@@ -1,9 +1,40 @@
 //arthimetic operators
-let a =5;
-let b = 2;
+// which need two object two perform operations.
+let a =10;
+let b =20;
 console.log("a + b =",a+b);
 console.log("a-b=",a-b);
 console.log("a*b=",a*b);
 console.log("a/b=",a/b);
 console.log("a%b=",a%b);
 console.log("a**b=",a**b); // 5^2
+
+// Unary operators.
+
+//1) increment
+//2) decrement
+
+//a) post increment.
+let x = 7;
+let y = 9;
+console.log("x=",x & "x=",x);
+console.log("x++=",x++);
+console.log("x=",x);
+
+//b) pre increment.
+
+console.log("x=",x & "y=",y);
+console.log("++x=",++x);
+console.log("x=",x);
+
+//a) post decrement.
+
+console.log("y=",y & "y=",y);
+console.log("y++=",y++);
+console.log("y=",y);
+
+//b) pre decrement.
+
+console.log("y=",y & "y=",y);
+console.log("++y=",++y);
+console.log("y=",y);
