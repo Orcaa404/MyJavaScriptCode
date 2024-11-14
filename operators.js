a98ba939478e7c7c530cdc7d72146969934a01ab
Orcaa404 <curiositycodea99@gmail.com>
2024-11-14 07:49:11 +0530

    update new changes

---
+++ operators.js
@@ -0,0 +1,9 @@
+//arthimetic operators
+let a =5;
+let b = 2;
+console.log("a + b =",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b); // 5^2
