4aeade227b9b1b4e8cfd6e71ee48b5c883a6d405
Orcaa404 <curiositycodea99@gmail.com>
2024-11-15 00:01:42 +0530

    operators

---
+++ comparision.js
@@ -0,0 +1,26 @@
+//comparison operator
+//== (equal to)
+//!= (not equal)
+//>
+//<
+// >=
+// <=
+//=== (equal to type)
+//!== (not equal to type)
+
+let a = 6;
+let b = 9;
+
+console.log("a==b",a==b);
+console.log("a!=b",a!=b);
+
+let u = 8;
+let p = "9";
+console.log("u>p=",u>p);
+console.log("u>=p=",u>=p);
+console.log("u<p=",u<p);
+console.log("u<=p=",u<=p);
+
+console.log("u===p",u===p);
+console.log("u!==p",u!==p);
+
---
+++ logical.js
@@ -0,0 +1,18 @@
+//Logical operators
+
+//1)Logical &&
+let a = 4;
+let b = 5;
+let condition1 = a<b;
+let condition2 = a===4;
+console.log("condition1 && condition2=",condition1 && condition2);
+
+//2)logical OR ||
+let A =9;
+let B = 79;
+let statement1 = A===B;
+let statement2 = A<B;
+console.log("statement1 || statement2=",statement1 || statement2);
+
+//logical not !
+console.log("!(A>B)=", !(A>B));
