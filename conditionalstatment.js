dea55dd0b95397badb1f069296ff133eef763c14
Orcaa404 <curiositycodea99@gmail.com>
2024-11-20 14:43:47 +0530

    conditional statement

---
+++ conditionalstatment.js
@@ -0,0 +1,126 @@
+//conditional statement.
+//1) if statement.
+let age = 18;
+if(age>=18){
+    console.log("you can vote");
+}
+if(age<18){
+    console.log("you can not vote");
+    }
+
+let mode = "dark";
+let color;
+if (mode === "dark"){
+    color = "black";
+}
+
+if(mode==="light"){
+    color = "white";
+}
+
+if (mode==="yellow"){
+    color ="yellow";
+}
+console.log(color);
+
+
+let colorOfCat = "red";
+let behavior;
+
+if(colorOfCat==="red"){
+    behavior ="angry type";
+}
+
+if(colorOfCat==="white"){
+behavior = "calm type";
+}
+
+if(colorOfCat==="black"){
+    behavior ="love to play type";
+}
+
+if(colorOfCat==="grey"){
+behavior ="sleepy type";
+}
+console.log(behavior);
+
+//2) if else statement.
+
+let studentAge = 20;
+if (studentAge<=20){
+    console.log("in third year");
+}
+else{
+    console.log("junior");
+}
+
+let apple = "kashmiri";
+if(apple= "kashmiri"){
+    console.log("indian apple.");
+}
+else{
+    console.log("from another country");
+}
+
+let simba_Lion_The_King = "cartoon movie";
+if(simba_Lion_The_King = "cartoon movie"){
+    console.log("watching on CN in my childhood");
+}
+else{
+    console.log("watching now into realistic animation");
+
+}
+
+//3) else if
+
+let Mode = "blue";
+let Color;
+
+if(Mode === "blue"){
+    Color ="zero harmful ray";
+} else if (Mode ==="dark"){
+    Color =" nigh vision";
+}
+else {
+Color = "high brightness";
+}
+console.log(Color);
+
+let audi =  "Absolutely beautiful";
+let other_Car;
+if (audi =  "Absolutely beautiful"){
+    other_Car ="are not first love";
+}
+else if(audi =  "is expensive"){
+    other_Car = "expensive as well";
+}
+else if(audi =  "symbol shine like a diamond")
+{
+    other_Car = "isn't attractive";
+} 
+else{
+    other_Car = "are not center of attention";
+}
+console.log(other_Car);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
